fa37f0eb08d11cf0570a7d13e23d7c711fe61323
Neil Kalman <neil.kalman@growthspace.com>
2024-06-26 04:31:09 +0300

    chore: add Calculator class with empty methods

---
+++ src/calculator.ts
@@ -0,0 +1,13 @@
+export class Calculator {
+  add(a: number, b: number) {
+  }
+
+  subtract(a: number, b: number) {
+  }
+
+  multiply(a: number, b: number) {
+  }
+
+  divide(a: number, b: number) {
+  }
+}
