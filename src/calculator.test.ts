15392a39308059f519048c3e6f63b4bb5f0ebbf5
Neil Kalman <neil.kalman@growthspace.com>
2024-06-26 04:34:20 +0300

    feat: implement divide method and test

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -13,5 +13,13 @@ describe('Calculator', () => {
     const calculator = new Calculator();
     expect(calculator.multiply(2, 3)).toBe(6);
   });
-  test.todo('divides 6 / 3 to equal 2');
+  test('divides 6 / 3 to equal 2', () => {
+    const calculator = new Calculator();
+    expect(calculator.divide(6, 3)).toBe(2);
+  });
+
+  test('throws error when dividing by zero', () => {
+    const calculator = new Calculator();
+    expect(() => calculator.divide(6, 0)).toThrow("Division by zero is not allowed.");
+  });
 });
