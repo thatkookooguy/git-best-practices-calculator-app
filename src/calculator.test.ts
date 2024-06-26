58dafecf7847065aa3c6c32d668e8f9622f77dd6
Neil Kalman <neil.kalman@growthspace.com>
2024-06-26 04:32:18 +0300

    feat: implement add method and test

--- src/calculator.test.ts
+++ src/calculator.test.ts
@@ -1,7 +1,10 @@
 import { Calculator } from '../src/calculator';
 
 describe('Calculator', () => {
-  test.todo('adds 2 + 3 to equal 5');
+  test('adds 2 + 3 to equal 5', () => {
+    const calculator = new Calculator();
+    expect(calculator.add(2, 3)).toBe(5);
+  });
   test.todo('subtracts 5 - 3 to equal 2');
   test.todo('multiplies 2 * 3 to equal 6');
   test.todo('divides 6 / 3 to equal 2');
