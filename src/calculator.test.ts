bbff504be672053a81475f25cd5eb77dbff522f8
Neil Kalman <neil.kalman@growthspace.com>
2024-06-26 04:31:22 +0300

    test: add test descriptions for Calculator methods

---
+++ src/calculator.test.ts
@@ -0,0 +1,8 @@
+import { Calculator } from '../src/calculator';
+
+describe('Calculator', () => {
+  test.todo('adds 2 + 3 to equal 5');
+  test.todo('subtracts 5 - 3 to equal 2');
+  test.todo('multiplies 2 * 3 to equal 6');
+  test.todo('divides 6 / 3 to equal 2');
+});
